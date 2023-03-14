b9e0dbb9ffbba614120d0002a4e84ea7acb83796
True._.Mary <truefalsemary@gmail.com>
2023-03-14 09:44:49 +0300

    adding cargo to navigation

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,23 @@ import './App.css';
 import {Library} from "./pages/library/library";
 import {CreateBook} from "./pages/library/create-book";
 import {BookPage} from "./pages/library/book-page";
+
 import {Carpark} from "./pages/carpark/carpark";
 import {CreateCar} from "./pages/carpark/create-car";
 import {CarPage} from "./pages/carpark/car-page";
+
 import {Cinema} from "./pages/cinema/cinema";
 import {CreateMovie} from "./pages/cinema/create-movie";
 import {MoviePage} from "./pages/cinema/movie-page";
+
 import {Storage} from "./pages/storage/storage";
 import {CreateDevice} from "./pages/storage/create-device";
 import {DevicePage} from "./pages/storage/device-page";
+
+
+import {Cargo, CargoTraffic} from "./pages/cargo-traffic/cargo-traffic";
+import {CreateCargo} from "./pages/cargo-traffic/create-cargo";
+import {CargoPage} from "./pages/cargo-traffic/cargo-page";
 
 function App() {
   return (
@@ -28,14 +36,17 @@ function App() {
         <Route path={'/carpark'} element={<Carpark />}/>
         <Route path={'/cinema'} element={<Cinema />}/>
         <Route path={'/storage'} element={<Storage />}/>
+          <Route path={'/cargo-traffic'} element={<CargoTraffic />}/>
         <Route path={'/create-book'} element={<CreateBook />}/>
         <Route path={'/create-car'} element={<CreateCar />}/>
         <Route path={'/create-movie'} element={<CreateMovie />}/>
         <Route path={'/create-device'} element={<CreateDevice />}/>
+          <Route path={'/create-cargo'} element={<CreateCargo />}/>
           <Route path={'/book-page/:id'} element={<BookPage />} />
           <Route path={'/car-page/:id'} element={<CarPage />} />
           <Route path={'/movie-page/:id'} element={<MoviePage />} />
           <Route path={'/device-page/:id'} element={<DevicePage />} />
+          <Route path={'/cargo-page/:id'} element={<CargoPage />} />
       </Routes>
     </div>
   );
